89a7857446af9ae2797ea4c9920c842ade5291a4
electrocodeur <77399487+electrocodeur@users.noreply.github.com>
2022-04-25 20:18:25 +0200

    Programme

---
+++ Blink_stm32.ino
@@ -0,0 +1,11 @@
+#define LED PB12
+void setup() {
+  pinMode(LED, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(LED, HIGH);
+  delay(1000);           
+  digitalWrite(LED, LOW);
+  delay(1000);           
+}
